147d5d57e6f345dce1abfaba3aad2a5018411e6b
GigaZelensky <85454363+GigaZelensky@users.noreply.github.com>
2025-11-20 22:18:42 +0000

    for the dumdums who cant install .NET
    title

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ set "APP_VERSION=1.0.1"
 set "OUT_NAME=DynamicEdge-%APP_VERSION%.exe"
 set "SRC_DIR=%~dp0src"
 set "MANIFEST=%~dp0app.manifest"
+set "CONFIG=%SRC_DIR%\app.config"
 echo ==========================================
 echo      Dynamic Edge Debug Builder
 echo ==========================================
@@ -22,12 +23,22 @@ if not exist "%MANIFEST%" (
 )
 
 echo Using manifest: %MANIFEST%
+echo Using config:   %CONFIG%
 echo Compiling...
 "%csc%" /target:winexe /unsafe /r:System.Windows.Forms.dll /r:System.Drawing.dll /r:System.Runtime.Serialization.dll /out:%OUT_NAME% /win32manifest:"%MANIFEST%" "%SRC_DIR%\\*.cs"
 
-if exist %OUT_NAME% (
+if exist "%CONFIG%" (
+    copy /Y "%CONFIG%" "%OUT_NAME%.config" >nul
+    echo Configuration file copied.
+)
+
+if exist "%OUT_NAME%" (
     echo.
-    echo Success! Run %OUT_NAME%
+    echo Success!
+    echo 1. %OUT_NAME%
+    if exist "%OUT_NAME%.config" echo 2. %OUT_NAME%.config
+    echo.
+    echo (Keep these files together)
 ) else (
     echo Build Failed.
 )
